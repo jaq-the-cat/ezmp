050aad690fb342831da777f117e60d5ec1e97a58
jaq-cat <jaq.cat@protonmail.ch>
2021-01-01 12:06:33 -0300

    yeet

--- lib/playlist.dart
+++ lib/playlist.dart
@@ -19,7 +19,24 @@ class PlaylistState extends State<PlaylistPage> {
                 title: Text(widget.name),
             ),
             body: ListView(
-                children: [],
+                children: widget.songs.map(
+                    (songName) {
+                        return Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                                Padding(
+                                    padding: EdgeInsets.only(left: 12.5),
+                                    child: Text(songName),
+                                ),
+                                IconButton(
+                                    icon: Icon(Icons.play_arrow),
+                                    color: Colors.green,
+                                    onPressed: () {},
+                                )
+                            ],
+                        );
+                    }
+                ).toList(),
             ),
             floatingActionButton: ElevatedButton.icon(
                 icon: Icon(Icons.music_note),
