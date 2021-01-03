848d812db8bacca695c4269197e067af2a927a44
jaq-cat <jaq.cat@protonmail.ch>
2021-01-02 23:07:20 -0300

    remove useless indent

--- lib/playlist.dart
+++ lib/playlist.dart
@@ -20,47 +20,50 @@ class PlaylistState extends State<PlaylistPage> {
             ),
             body: ListView(
                 children: <Widget>[
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                            ElevatedButton.icon(
-                                icon: Icon(Icons.shuffle),
-                                label: Text("Shuffle"),
-                                onPressed: () {},
-                            ),
-                            SizedBox(width: 5),
-                            ElevatedButton.icon(
-                                icon: Icon(Icons.play_arrow),
-                                label: Text("Play"),
-                                onPressed: () {},
-                            ),
-                        ]
-                    )
+                    Container(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                                ElevatedButton.icon(
+                                    icon: Icon(Icons.shuffle),
+                                    label: Text("Shuffle"),
+                                    onPressed: () {},
+                                ),
+                                SizedBox(width: 5),
+                                ElevatedButton.icon(
+                                    icon: Icon(Icons.play_arrow),
+                                    label: Text("Play"),
+                                    onPressed: () {},
+                                ),
+                            ]
+                        )
+                    ),
                 ] + widget.songs.map(
-                    (songName) {
-                        return Padding(
-                            padding: EdgeInsets.only(left: 12.5),
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                    Text(
-                                        songName,
-                                        style: TextStyle(
-                                            fontSize: 17.5,
-                                            color: Colors.black54,
-                                        )
-                                    ),
-                                    Text(
-                                        "XX:XX",
-                                        style: TextStyle(
-                                            fontSize: 12,
-                                            color: Colors.black54,
-                                        )
-                                    ),
-                                ],
-                            ),
-                        );
-                    }
+                    (songName) => Container(
+                        margin: EdgeInsets.all(10),
+                        padding: EdgeInsets.only(left: 5),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                                Text(
+                                    songName,
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.white,
+                                    )
+                                ),
+                                Text(
+                                    "XX:XX",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        color: Colors.white38,
+                                    )
+                                ),
+                            ],
+                        ),
+                    ),
                 ).toList(),
             ),
             floatingActionButton:  ElevatedButton.icon(
